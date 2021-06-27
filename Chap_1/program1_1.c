7c376ab39425f0f482fa880359126ebf3c3a2096
Weilin Luo <weilin.luo@thundersoft.com>
2021-06-26 19:49:37 -0700

    program1.1: add comments

--- Chap_1/program1_1.c
+++ Chap_1/program1_1.c
@@ -7,6 +7,18 @@
     has an entry for each object, with the property that
     id[p] and id[q] are equal if and only if p and q are
     connected.
+
+Psudo code:
+1. for i = 0 to id.length-1
+2.     id[i] = i
+3. while there is valid pair: p q
+4.     if id[p] == id[q]    // if p and q are already connected - this is why this algorithm is called quick-find: very easy
+5.         continue         // to check if two objects are already connected or not.
+6.     for i to id.length-1
+7.         if id[i] == id[p]
+8.             id[i] = id[q]    // if any object is connected to p, connect it to q also
+9.     print p and q    // print out pair representing objects that are not yet connected
+
 */
 
 #include <stdio.h>
